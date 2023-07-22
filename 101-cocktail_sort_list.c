1c99db24dea76e173e34115376158dad6eafb99e
MsVron <126300537+MsVron@users.noreply.github.com>
2023-07-22 02:38:53 +0100

    Cocktail shaker sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -34,47 +34,36 @@ void swap_nodes(listint_t **list, listint_t *a, listint_t *b)
  *in ascending order using the Cocktail shaker sort algorithm
  *@list: Pointer to the head of the list
  */
+/**
+ *cocktail_sort_list - sorts a doubly linked list of integers
+ *in ascending order using the Cocktail shaker sort algorithm
+ *@list: Pointer to the head of the list
+ */
 void cocktail_sort_list(listint_t **list)
 {
 	char swapped = 1;
-	listint_t * current;
-
-	if (!list || ! *list)
-		return;
-
+	listint_t *current;
+	if (!list || !*list) return;
 	current = *list;
-
-	while (swapped)
-	{
+	while (swapped) {
 		swapped = 0;
-
-		while (current->next)
-		{
-			if (current->n > current->next->n)
-			{
+		while (current->next) {
+			if (current->n > current->next->n) {
 				swap_nodes(list, current, current->next);
 				swapped = 1;
 				print_list((const listint_t *) *list);
 			}
-			else
-				current = current->next;
+			else current = current->next;
 		}
-
-		if (!swapped)
-			break;
-
+		if (!swapped) break;
 		swapped = 0;
-
-		while (current->prev)
-		{
-			if (current->n < current->prev->n)
-			{
+		while (current->prev) {
+			if (current->n < current->prev->n) {
 				swap_nodes(list, current->prev, current);
 				swapped = 1;
 				print_list((const listint_t *) *list);
 			}
-			else
-				current = current->prev;
+			else current = current->prev;
 		}
 	}
 }
