1fef4b04d5caba9e927d8773180b249b3c988930
MsVron <126300537+MsVron@users.noreply.github.com>
2023-07-22 15:31:13 +0100

    sort.h

--- sort.h
+++ sort.h
@@ -21,7 +21,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
-void swap_nodes(listint_t **list, listint_t *a, listint_t *b
+void swap_nodes(listint_t **list, listint_t *a, listint_t *b);
 void counting_sort(int *array, size_t size);
 
 #endif /* SORT_H */
