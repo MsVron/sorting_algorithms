ee0439faed9358189d296e6d2f71a3e0753496cd
MsVron <126300537+MsVron@users.noreply.github.com>
2023-07-22 01:30:42 +0100

    sort.h

--- sort.h
+++ sort.h
@@ -5,7 +5,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* Struct for doubly linked list */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
 void bubble_sort(int *array, size_t size);
 void print_array(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void print_list(const listint_t *list);
 
 #endif /* SORT_H */
