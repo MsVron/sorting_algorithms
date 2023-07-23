2ee568ffe7e0a7e853cde122601a6cad67901121
MsVron <126300537+MsVron@users.noreply.github.com>
2023-07-23 14:38:04 +0100

    Counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -31,7 +31,11 @@ void counting_sort(int *array, size_t size)
 	for (i = 0; i < (int) size; i++)
 		count[array[i]]++;
 
-	/*Print counting array */
+	/*Transform count into cumulative count */
+	for (i = 1; i <= max; i++)
+		count[i] += count[i - 1];
+
+	/*Print cumulative count array */
 	for (i = 0; i <= max; i++)
 	{
 		printf("%d", count[i]);
@@ -41,11 +45,26 @@ void counting_sort(int *array, size_t size)
 
 	printf("\n");
 
-	/*Reconstruct the sorted array */
-	for (i = 0; i <= max; i++)
-		while (count[i]-- > 0)
-			array[k++] = i;
+	/*Allocate memory for the output (sorted array) */
+	int *output = malloc(size* sizeof(int));
+	if (!output)
+	{
+		free(count);
+		return;
+	}
 
-	/*Free the count array */
+	/*Build output array */
+	for (i = 0; i < (int) size; i++)
+	{
+		output[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
+	}
+
+	/*Copy output array to the original array */
+	for (i = 0; i < (int) size; i++)
+		array[i] = output[i];
+
+	/*Free count and output arrays */
 	free(count);
+	free(output);
 }
