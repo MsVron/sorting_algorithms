771cc4cf22a6d49d77d4253f4367497b548b8267
MsVron <126300537+MsVron@users.noreply.github.com>
2023-07-24 13:08:52 +0100

    sort.h

--- sort.h
+++ sort.h
@@ -25,5 +25,8 @@ void swap_nodes(listint_t **list, listint_t *a, listint_t *b);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void merge(int *array, int size, int mid, int *temp);
+void sift_down(int *array, size_t start, size_t end, size_t size);
+void heapify(int *array, size_t size);
+void heap_sort(int *array, size_t size);
 
 #endif /* SORT_H */
