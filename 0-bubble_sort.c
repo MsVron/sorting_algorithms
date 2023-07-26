ac65d7957ead8bf5a422fd4165e85930e0240a4e
MsVron <126300537+MsVron@users.noreply.github.com>
2023-07-26 14:09:49 +0100

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,12 +14,18 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
+	/*Check if the array is NULL */
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
-			{	/*Swap array[j] and array[j + 1] */
+			{ /*Swap array[j] and array[j + 1] */
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
